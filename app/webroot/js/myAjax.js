7adc5b3ae32c7e7ee21bd5da3d33efabb7aff3e3
elbixos <vpage.vincent@gmail.com>
2015-02-11 09:08:49 -0400

    Ajax ProdLines ok
    Premier appel ajax operationnel :
    - un click sur 'Voir' sous une ProdLine appelle un controller specifique
    pour la prodLine et renvoie la vue associée
    - celle ci est inseree dans une div vide au départ

--- app/webroot/js/myAjax.js
+++ app/webroot/js/myAjax.js
@@ -1,21 +1,22 @@
-$( "#listeProdFromProductLine" ).html( "Next Step..." );
+//$( "#listeProdFromProductLine" ).html( "Next Step..." );
 //alert('boom');
 
-$('#testclick').on('click', function(event){
-	//alert('clicked');
-	$.ajax({
-            dataType: "html",
-            type: "POST",
-            evalScripts: true,
-            url: '/cakeGit/products/viewAjaxProductsFromProductLine/1',
-			//url: '/cakeGit/product_lines/mytest',
-            data: ({type:'original'}),
-            success: function (data, textStatus){
-                $("#listeProdFromProductLine").html(data);
+$( document ).ready(function() {
+	$('.testclick').on('click', function(event){
+		var pl_id = $(this).attr('product_line_id');
+		//alert('clicked '+pl_id);
+		$.ajax({
+				dataType: "html",
+				type: "POST",
+				evalScripts: true,
+				url: '/cakeGit/products/viewAjaxProductsFromProductLine/'+pl_id,
+				//url: '/cakeGit/product_lines/mytest',
+				data: ({type:'original'}),
+				success: function (data, status){
+					//alert(event.target);
+					$("#listeProdFromProductLine").html(data);
 
-            }
-        });
+				}
+			});
+	});
 });
-/*
-
-*/
